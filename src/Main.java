51ffa0760b5e1bba267cc7b200c479ebb43214d7
IrenaMika <ira.mika@bk.ru>
2024-02-20 21:24:21 +0300

    tap

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 23570; // стоимость билета
-        int bonus = 20; // стоимость мили
-        System.out.println("При покупке авиабилета начисляются мили. При покупке одного билета начисляется:");
-        System.out.println((ticket / bonus) + " милей");
+        System.out.println("Hello world!");
     }
 }
