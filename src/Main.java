81dc6d854502201a7b1cb839dcdbfb8d954b0650
IrenaMika <ira.mika@bk.ru>
2024-03-24 20:04:05 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
-public static void main(String[] args) {
-int ticket = 23570; // стоимость билета
-int bonus = 20; // стоимость мили
-System.out.println("При покупке авиабилета начисляются мили. При покупке одного билета получается:");
-System.out.println((ticket / bonus) + " милей");
-}
+    public static void main(String[] args) {
+        int ticket = 23570; // стоимость билета
+        int bonus = 20; // стоимость мили
+        System.out.println("При покупке авиабилета начисляются мили. При покупке одного билета получается:");
+        System.out.println((ticket / bonus) + " милей");
+    }
 }
